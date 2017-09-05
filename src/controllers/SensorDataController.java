8209cb9275d1919a49436f85fe8707daedb53eb2
Thomas Rokicki <TomWRHS@gmail.com>
2017-09-04 23:46:51 -0400

    Removed Uneeded Endpoints
    Currently do not need an end point to return what raspberry pi or arduino a sensor is on based on the raspberry pi and arduino end point ... kinda dead code.
    So I removed it.

--- src/controllers/SensorDataController.java
+++ src/controllers/SensorDataController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import handlers.TeardownHandler;
+import handlers.SensorPayloadHandler;
+import payloads.SensorDataPayload;
 import payloads.SensorDataResponse;
 
 /**
@@ -34,39 +35,6 @@ public class SensorDataController {
 		return "Test";
 	}
 	
-	/**
-	 * Handles a GET request via id of a sensor (Example).
-	 * 
-	 * @param id
-	 *            the identification.
-	 * @return an HTTP status code.
-	 */
-	@RequestMapping(value = "/sensorData/{id}", method = RequestMethod.GET)
-	public ResponseEntity<SensorDataResponse> get(@PathVariable String id) {
-
-		// return response
-		SensorDataResponse res = new SensorDataResponse();
-		res.setValue(id);
-		return new ResponseEntity<SensorDataResponse>(res, HttpStatus.OK);
-	}
-
-	/**
-	 * Handles a GET request based on a specific sensor.
-	 * 
-	 * @param ard
-	 *            the name of the arduino the sensor belongs to.
-	 * @param sens
-	 *            the specific sensors name.
-	 * @return an HTTP status code.
-	 */
-	@RequestMapping(value = "/sensorData/{ard}/{sens}", method = RequestMethod.GET)
-	public ResponseEntity<SensorDataResponse> get(@PathVariable String ard, @PathVariable String sens) {
-
-		// return response
-		SensorDataResponse res = new SensorDataResponse();
-		res.setValue(sens);
-		return new ResponseEntity<SensorDataResponse>(res, HttpStatus.OK);
-	}
 
 	/**
 	 * The is how the data is posted the database. The Raspberry Pi is what is
@@ -79,11 +47,18 @@ public class SensorDataController {
 	 */
 	@RequestMapping(value = "/sensorData", method = RequestMethod.POST)
 	public ResponseEntity<SensorDataResponse> recieveSensorData(@RequestBody String req) {
+
+		//Send pay load to be added to a queue
+		SensorDataPayload payload = new SensorDataPayload();
+		payload.setBody(req);
 		
+		// Takes the pay load, converts it to SensorData, then adds to the queue
+		SensorPayloadHandler payloadHandler = new SensorPayloadHandler(payload);
+		payloadHandler.handleSensorPayload();
+		
+		// Creates a response to send back to the caller
 		SensorDataResponse res = new SensorDataResponse();
-		res.setValue(req);
-		TeardownHandler tearHandler = new TeardownHandler();
-		tearHandler.parseSensorJson(req);
+		res.setBody(req);
 		return new ResponseEntity<SensorDataResponse>(res, HttpStatus.OK);
 	}
 
